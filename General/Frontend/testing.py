b0f5eca6fb2af0082d1a5cc3d4ec35bbc2d2f2cc
marius-steinmann-git <marius.steinmann.git@gmail.com>
2026-07-15 22:52:30 +0200

    pandas

--- General/Frontend/testing.py
+++ General/Frontend/testing.py
@@ -8,4 +8,11 @@ s = pd.Series([1, 3, 5, np.nan, 6, 8])
 print (s)
 
 
-
+df2 = pd.DataFrame(
+    {
+        "A": 1.0,
+        "B": pd.Timestamp("20130102"),
+        "C": pd.Series(1, index = list(range(4)), dtype="float32"),
+    }
+)
+print(df2)
